80b36c53e3e436bac8882227e43c11b8cd51a63b
avosa <websterb17@gmail.com>
2020-11-03 00:56:01 +0300

    Ädded new files

---
+++ questions.js
@@ -0,0 +1,47 @@
+/*
+ * Question sources
+ * https://www.princetonreview.com/med-school-advice/medical-school-interview-questions
+ */
+
+const QUESTIONS = [
+    "Tell me about yourself",
+    "How would you describe yourself?",
+    "What makes you unique?",
+    "How do you envision using your medical education?",
+    "Why do you want to work here?",
+    "What interests you about this role?",
+    "What motivates you?",
+    "What are you passionate about?",
+    "Why are you leaving your current job?",
+    "What do you do for fun?",
+    "What are your greatest strengths?",
+    "What your greatest weaknesses?",
+    "What are your goals for the future?",
+    "Can you tell me about a difficult work situation and how you overcame it?",
+    "What is your salary range expectation?",
+    "Why should we hire you?",
+    "Do you have any questions?",
+    "What did you like most about your last position?",
+    "What did you like least about your last position?",
+    "How do you handle stress?",
+    "What is your greatest accomplishment?",
+    "What is your teaching philosophy?",
+    "What does customer service mean to you?",
+    "Tell me about your work experience?",
+    "How do you define success?",
+    "How do you work under pressure?",
+    "What is your dream job?",
+    "What can you bring to the company?",
+    "How do you handle conflict at work?",
+    "Why are you interested in this position?",
+    "What skills would you bring to the job?",
+    "Can you explain these gaps in your resume?",
+    "Would you be willing to work nights and weekends?",
+    "Describe a time when your boss was wrong. How did you handle the situation?",
+    "What is your ideal working environment?",
+    "Do you have a personal mission statement?",
+    "If you suddenly gained the ability to time travel, what’s the first thing you’d do?",
+    "Is it better to be good and on time or perfect and late with your work?",
+    "Do you have a mentor?"
+];
+
